34d150947d39499a784573f8fc439cc2c0aff9bf
GuiChambel <babyyodaamigo@gmail.com>
2022-03-12 14:32:38 +0000

    Added stuff

--- main.py
+++ main.py
@@ -1,121 +1,103 @@
+from asyncio.windows_events import NULL
+from gettext import NullTranslations
 import numpy as np
 import cv2
 import pyautogui
 import discord
 import json
-f = open ('data/data.json', "r+")
-token2 = json.loads(f.read())
-token1 = token2["token"]
-
 from tkinter import *
-bot = discord.Client()
-code = False
-
-
+window = Tk()
+window.title("Fortnite")
+send = NULL
 def screenshot():
     image = pyautogui.screenshot()
     image = cv2.cvtColor(np.array(image),cv2.COLOR_RGB2BGR)
     cv2.imwrite("./output/code.png", image)
 
-while code == True:
-    group = pyautogui.locateOnScreen('./input/portuguese/group.png', confidence=.6)
-    if group == None:
-        print('Not found')
-    else: 
-        pyautogui.moveTo(group, duration=.1)
-        pyautogui.click(interval=.3)
-        print('Found')
-        code = False
-        code1 = True
-    
 
-while code1 == True:
-    create = pyautogui.locateOnScreen('./input/portuguese/create.png', confidence=.6)
-    if create == None:
-        print('Not Found')
-    else: 
-        pyautogui.moveTo(create, duration=.1)
-        pyautogui.click(interval=.3)
-        print('Found')
-        code1 = False
-        code2 = True
-
-while code2 == True:
-    seecode = pyautogui.locateOnScreen('./input/portuguese/inicialcode.png', confidence=.7)
-    if seecode is None:
-        print('Dont Found')
-    else:
-        print('Found')
-        screenshot()
-        code2 = False
-        send = False
-        loading = True
-
-while loading == True:
-    loading2 = pyautogui.locateOnScreen('./input/portuguese/loading.png', confidence=.7)
-    if seecode is None:
-        print('Dont Found')
-    else:
-        print('Found match')
-        loading2 = False
-        icy = True
-        
-@bot.event
-async def on_ready():
-    if send == False:
-        channel = bot.get_channel(channeltosend)
-        await channel.send(file = discord.File("./output/code.png"))
-        
-
-
-
-
-
-window = Tk()
-window.title("Fortnite")
-mapa = None
-
-
+def start():
+    print('not working')
 def display():
-    if(x1.get()==1):
+    if(sendtodiscord.get()==1):
         print(1)
     else:
         print(0)
-
-def submit():
-    token = entrytoken.get()
-    entrytoken.config(state=DISABLED)
-    tokentoput = {"token": token}
-    jsonString = json.dumps(tokentoput)
-    jsonFile = open("data/data.json", "w")
-    jsonFile.write(jsonString)
-    jsonFile.close()
-x1 = IntVar()
-
-check_button = Checkbutton(window,
-                           text="I agree to something",
-                           variable=x1,
-                           onvalue=1,
-                           offvalue=0,
-                           command=display,
-                           font=('Arial',20),
-                           compound='left')
-check_button.pack()
-
-
-
-entrytoken = Entry(window,
-              font=("Arial",50),
-              fg="#00FF00",
-              bg="black",
+def locatecode():
+        seecode = pyautogui.locateOnScreen('./input/portuguese/inicialcode.png', confidence=.7)
+        if seecode == None:
+            print(seecode)
+        else:
+            print(seecode)
+            print('Found')
+            screenshot()
+            code2 = False
+            global send
+            send = False
+        
+def sendtodiscord():
+    @bot.event
+    async def on_ready():
+        global send
+        if send == False:
+            channel = bot.get_channel()
+            await channel.send(file = discord.File("./output/code.png"))
+        
+def config():
+    def submit():
+        token = entrytoken.get()
+        channel1 = channel.get()
+        entrytoken.config(state=DISABLED)
+        channel.config(state=DISABLED)
+        if token is None:
+            print('f')
+        if channel1 is None:
+            print('f32424')
+        tokenchanneltoput = {"token": token,"channel": channel1}
+        jsonString = json.dumps(tokenchanneltoput)
+        jsonFile = open("data/data.json", "w")
+        jsonFile.write(jsonString)
+        jsonFile.close()
+        window_config.destroy()
+    window_config = Tk()
+    entrytoken = Entry(window_config,
+              font=("Arial",15),
+              fg="#000000",
               show="*"
               )
 
-entrytoken.pack(side=LEFT)
+    entrytoken.pack(side=LEFT)
+    channel = Entry(window_config,
+              font=("Arial",15),
+              fg="#000000",
+              show="*"
+              )
+    channel.pack(side=LEFT)
+    submit_button = Button(window_config,text="submit",command=submit)
+    submit_button.pack(side=RIGHT)
 
-submit_button = Button(window,text="submit",command=submit)
-submit_button.pack(side=RIGHT)
 
+sendtodiscord = IntVar()
+Button(window,text="config",command=config).pack()
+
+start = Button(window,text="start",command=locatecode)
+start.pack()
+
+check_button = Checkbutton(window,
+                           text="Send to discord",
+                           variable=sendtodiscord,
+                           onvalue=1,
+                           offvalue=0,
+                           command=display,
+                           font=('Arial',12),
+                           compound='left')
+check_button.pack()
+
+bot = discord.Client()
+f = open ('data/data.json', "r+")
+datas = json.loads(f.read())
+token1 = datas["token"]
+token = token1
+channel1 = datas["channel"]
+channel = channel1
+#bot.run(token)
 window.mainloop()
-
-bot.run(token1)
